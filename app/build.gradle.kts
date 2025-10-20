6072883aea84b4c13260b688bec0a4b76b2ff86a
BTShinc <sangp3328@gmail.com>
2025-10-20 22:35:20 +0700

    update token get userId

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.okhttp3:logging-interceptor:3.14.9")
+    implementation("com.auth0:java-jwt:4.4.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
