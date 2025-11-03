3e6dbbcc985dc73a3a8abed6af8e379b10a6df25
BTShinc <sangp3328@gmail.com>
2025-11-03 16:47:08 +0700

    update list food admin

--- app/src/main/java/com/example/prm392_assignment_food/data/network/ApiService.java
+++ app/src/main/java/com/example/prm392_assignment_food/data/network/ApiService.java
@@ -164,4 +164,7 @@ public interface ApiService {
             @Query("orderId") String orderId,
             @Query("orderStatus") String status
     );
+
+    @GET("/api/admin/wallet")
+    Call<Double> getAdminWallet();
 }
